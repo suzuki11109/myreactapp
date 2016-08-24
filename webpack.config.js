877266daf5dcb2fce4fe28362cd2bcfe5d65f62a
Suzuki Aki <suzuki11109@gmail.com>
2016-08-24 11:18:16 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts', '.tsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.tsx?$/,
+      loaders: ['awesome-typescript-loader'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
