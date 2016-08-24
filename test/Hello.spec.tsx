3e8f12f7fd6dc643cf9eb15eeed423fc3efa99ec
Suzuki Aki <suzuki11109@gmail.com>
2016-08-24 11:50:05 +0700

    setup sample mocha test

---
+++ test/Hello.spec.tsx
@@ -0,0 +1,9 @@
+import {expect} from "chai";
+
+describe("sample test", () => {
+  it("setup", () => {
+    expect(true).to.be.false;
+  });
+});
+
+
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,6 @@
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/enzyme/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/react-addons-test-utils/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
